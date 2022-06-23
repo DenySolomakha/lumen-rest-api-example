9360d25b196dbd8ccd80bd0f266dcc237622d1af
Denys Solomakha <denys.service@ukr.net>
2022-06-24 01:04:33 +0300

    define auth routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+/** @var Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use App\Http\Controllers\Auth\SignIn;
+use App\Http\Controllers\Auth\SignUp;
+use Illuminate\Support\Str;
+use Laravel\Lumen\Routing\Router;
 
-$router->get('/', function () use ($router) {
+$router->get('/', function () use ($router): string {
     return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function (Router $router): void {
+    // Generate random string for application key
+    $router->get('appKey', fn ():string => Str::random(32));
+
+    // Authentication
+    $router->post('signUp', ['as' => 'SignUp', 'uses' => SignUp::class]);
+    $router->post('signIn', ['as' => 'SignIn', 'uses' => SignIn::class]);
 });
