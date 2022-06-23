8e3fa040f6dc56b78d1eb3926d643fe0fa77e966
Denys Solomakha <denys.service@ukr.net>
2022-06-24 01:12:18 +0300

    improve naming

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,8 +2,8 @@
 
 /** @var Router $router */
 
-use App\Http\Controllers\Auth\SignIn;
-use App\Http\Controllers\Auth\SignUp;
+use App\Http\Controllers\Auth\SignInAction;
+use App\Http\Controllers\Auth\SignUpAction;
 use Illuminate\Support\Str;
 use Laravel\Lumen\Routing\Router;
 
@@ -16,6 +16,6 @@ $router->group(['prefix' => 'api'], function (Router $router): void {
     $router->get('appKey', fn ():string => Str::random(32));
 
     // Authentication
-    $router->post('signUp', ['as' => 'SignUp', 'uses' => SignUp::class]);
-    $router->post('signIn', ['as' => 'SignIn', 'uses' => SignIn::class]);
+    $router->post('signUp', ['as' => 'SignUp', 'uses' => SignUpAction::class]);
+    $router->post('signIn', ['as' => 'SignIn', 'uses' => SignInAction::class]);
 });
