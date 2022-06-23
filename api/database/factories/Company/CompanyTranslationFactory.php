38ceb59dce1081e60be55ce3e0154b6cfab46ac6
Denys Solomakha <denys.service@ukr.net>
2022-06-24 01:16:29 +0300

    created CompanyTranslationFactory

---
+++ api/database/factories/Company/CompanyTranslationFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\Company;
+
+use App\Models\Company\Company;
+use App\Models\Company\CompanyTranslation;
+use App\Models\Language;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyTranslationFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = CompanyTranslation::class;
+
+    /**
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'company_id' => Company::factory(),
+            'language_id' => Language::factory(),
+            'title' => $this->faker->company(),
+            'description' => $this->faker->realText(700),
+            'meta_title' => sprintf('%s | %s', $this->faker->company(), env('APP_NAME')),
+            'meta_description' => sprintf('%s | %s', $this->faker->realText(50), env('APP_NAME')),
+        ];
+    }
+}
