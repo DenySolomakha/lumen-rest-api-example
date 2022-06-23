71282a30e0fb68d786269617a96bf3d3c2aeaaa8
Denys Solomakha <denys.service@ukr.net>
2022-06-24 01:09:35 +0300

    created CompanyFactory

---
+++ api/database/factories/Company/CompanyFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\Company;
+
+use App\Models\Company\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'number_of_employees' => $this->faker->numberBetween(5, 3500),
+            'slug' => $this->faker->slug,
+        ];
+    }
+}
