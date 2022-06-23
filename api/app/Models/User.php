a6202242e3311ecaab4330c5b439107aed648edb
Denys Solomakha <denys.service@ukr.net>
2022-06-24 01:10:13 +0300

    added relation to companies

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -1,14 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
+use App\Models\Company\Company;
+use Database\Factories\UserFactory;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Lumen\Auth\Authorizable;
 
+/**
+ * @method static UserFactory factory(...$parameters)
+ */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
@@ -30,4 +38,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function companies(): HasMany
+    {
+        return $this->hasMany(Company::class);
+    }
 }
