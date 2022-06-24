57db8e27cbcfa4af423a6f4232eeccfc20772ab7
Denys Solomakha <denys.service@ukr.net>
2022-06-24 07:54:52 +0300

    seeder for default data

--- api/database/seeders/UserTableSeeder.php
+++ api/database/seeders/UserTableSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Company\Company;
 use App\Models\Company\CompanyTranslation;
-use App\Models\Language;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder
@@ -20,12 +20,19 @@ class UserTableSeeder extends Seeder
     {
         User::factory()
             ->count(5)
-            ->has(Company::factory()
-                ->count(3)
-                ->has(CompanyTranslation::factory()
-                    ->count(3)->has(Language::factory()
-                        ->count(3), 'language'), 'translations'
-                ), 'companies')
+            ->has(
+                Company::factory()
+                    ->count(3)
+                    ->has(
+                        CompanyTranslation::factory()
+                            ->count(2)
+                            ->state(new Sequence(
+                                    ['language_id' => 1],
+                                    ['language_id' => 2],
+                                )
+                            ),
+                        'translations'),
+                'companies')
             ->create();
     }
 }
