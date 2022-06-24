92968c8ede935110c935f572fdb87c12428e7547
Denys Solomakha <denys.service@ukr.net>
2022-06-24 07:53:27 +0300

    created LanguageFactory

---
+++ api/database/factories/LanguageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Language;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LanguageFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Language::class;
+
+    /**
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->country,
+            'code' => $this->faker->languageCode,
+            'is_active' => true,
+        ];
+    }
+}
