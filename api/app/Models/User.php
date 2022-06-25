ab723b85ac2c3b84a9fbb0708d67e172acdb0357
Denys Solomakha <denys.service@ukr.net>
2022-06-25 18:19:45 +0300

    use CanResetPasswordTrait, Notifiable

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -9,17 +9,32 @@ use Database\Factories\UserFactory;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordInterface;
 use Laravel\Lumen\Auth\Authorizable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+
 
 /**
+ * @property int $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property string $token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @method static UserFactory factory(...$parameters)
+ * @method static Builder|User query()
  */
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject, CanResetPasswordInterface
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable, Authorizable, HasFactory, CanResetPasswordTrait, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -27,7 +42,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string[]
      */
     protected $fillable = [
-        'name', 'email',
+        'username', 'email',
     ];
 
     /**
@@ -39,8 +54,27 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'password',
     ];
 
+    /**
+     * @return HasMany
+     */
     public function companies(): HasMany
     {
         return $this->hasMany(Company::class);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
     }
 }
