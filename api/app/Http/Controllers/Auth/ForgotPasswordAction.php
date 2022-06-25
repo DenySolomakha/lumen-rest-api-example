1dc0dbdca53c1494ac8246b4e1e22be94bdd30ee
Denys Solomakha <denys.service@ukr.net>
2022-06-25 18:17:52 +0300

    created ForgotPasswordAction

---
+++ api/app/Http/Controllers/Auth/ForgotPasswordAction.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Validators\Auth\ForgotPasswordValidator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForgotPasswordAction extends Controller
+{
+    use ForgotPasswordValidator;
+
+    /**
+     * @throws ValidationException
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $this->validateRequest($request);
+
+        $status = Password::sendResetLink($request->only('email'));
+
+        return $status === Password::RESET_LINK_SENT ?
+            response()->json(['data' => 'Success.']) :
+            response()->json(['data' => 'Error.'], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
