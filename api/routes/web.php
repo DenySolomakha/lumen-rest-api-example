646c9bd4c09a967f43fef605b2c199c64f7d3fef
Denys Solomakha <denys.service@ukr.net>
2022-06-25 18:19:18 +0300

    added new routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,8 +2,11 @@
 
 /** @var Router $router */
 
+use App\Http\Controllers\Auth\ForgotPasswordAction;
+use App\Http\Controllers\Auth\ResetPasswordAction;
 use App\Http\Controllers\Auth\SignInAction;
 use App\Http\Controllers\Auth\SignUpAction;
+use App\Http\Controllers\Company\CompanyAction;
 use Illuminate\Support\Str;
 use Laravel\Lumen\Routing\Router;
 
@@ -16,6 +19,14 @@ $router->group(['prefix' => 'api'], function (Router $router): void {
     $router->get('appKey', fn ():string => Str::random(32));
 
     // Authentication
-    $router->post('signUp', ['as' => 'SignUp', 'uses' => SignUpAction::class]);
-    $router->post('signIn', ['as' => 'SignIn', 'uses' => SignInAction::class]);
+    $router->post('signUp', ['as' => 'signUp', 'uses' => SignUpAction::class]);
+    $router->post('signIn', ['as' => 'signIn', 'uses' => SignInAction::class]);
+
+    // Forgot password
+    $router->post('forgot-password', ['as' => 'forgot.password', 'uses' => ForgotPasswordAction::class]);
+    $router->patch('reset-password', ['as' => 'reset.password', 'uses' => ResetPasswordAction::class]);
+
+    $router->group(['middleware' => 'auth'], function (Router $router): void {
+        $router->get('users/companies', ['as' => 'users.companies', 'uses' => CompanyAction::class]);
+    });
 });
