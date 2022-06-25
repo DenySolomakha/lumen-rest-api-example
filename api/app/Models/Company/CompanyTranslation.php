4c022caa7a230d207d0511ac233c9a8a5d697504
Denys Solomakha <denys.service@ukr.net>
2022-06-25 20:15:46 +0300

    added property for ide

--- api/app/Models/Company/CompanyTranslation.php
+++ api/app/Models/Company/CompanyTranslation.php
@@ -9,8 +9,18 @@ use Database\Factories\Company\CompanyTranslationFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
+ * @property int $id
+ * @property int $company_id
+ * @property string $language
+ * @property string $title
+ * @property string $description
+ * @property string $meta_title
+ * @property string $meta_description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @method static CompanyTranslationFactory factory(...$parameters)
  */
 class CompanyTranslation extends Model
@@ -21,8 +31,11 @@ class CompanyTranslation extends Model
      * @var array
      */
     protected $fillable = [
+        'language',
         'title',
         'description',
+        'meta_title',
+        'meta_description',
     ];
 
     /**
@@ -46,6 +59,6 @@ class CompanyTranslation extends Model
      */
     public function language(): BelongsTo
     {
-        return $this->belongsTo(Language::class);
+        return $this->belongsTo(Language::class, 'language');
     }
 }
