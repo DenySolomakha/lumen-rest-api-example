b88f4b43a4f5c1a0184382e58e2a51f414d5b5a6
Denys Solomakha <denys.service@ukr.net>
2022-06-25 18:20:15 +0300

    created SignInAction

--- api/app/Http/Controllers/Auth/SignInAction.php
+++ api/app/Http/Controllers/Auth/SignInAction.php
@@ -5,12 +5,35 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Http\Validators\Auth\SingInValidator;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 final class SignInAction extends Controller
 {
-    public function __invoke(Request $request)
+    use SingInValidator;
+
+    /**
+     * @throws ValidationException
+     */
+    public function __invoke(Request $request): UserResource|JsonResponse
     {
-       dd($request->all());
+        $this->validateRequest($request);
+
+        if(!$token = auth()->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'data' => 'Invalid credentials.'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        /** @var User $user */
+        $user = auth()->user();
+        $user->token = $token;
+
+        return new UserResource(auth()->user());
     }
 }
