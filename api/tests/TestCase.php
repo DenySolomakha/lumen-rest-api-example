7a5ac82153bc7f690ae41ef419702190ee273d6b
Denys Solomakha <denys.service@ukr.net>
2022-07-05 00:22:44 +0300

    low refactor

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
+use Laravel\Lumen\Application;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,9 +12,9 @@ abstract class TestCase extends BaseTestCase
     /**
      * Creates the application.
      *
-     * @return \Laravel\Lumen\Application
+     * @return Application
      */
-    public function createApplication()
+    public function createApplication(): Application
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
